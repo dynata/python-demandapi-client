b85800bd5a5741ef0c0efa4d510e84e9d2087e1b
Bradley Wogsland <bradley@wogsland.org>
2019-12-06 08:11:14 -0600

    POP-2381 get line item detailed report

--- tests/test_lineitems.py
+++ tests/test_lineitems.py
@@ -21,10 +21,19 @@ class TestLineItemEndpoints(unittest.TestCase):
         self.api._access_token = 'Bearer testtoken'
 
     @responses.activate
-    def test_get_lineitem(self):
-        with open('./tests/test_files/get_lineitem.json', 'r') as lineitem_file:
-            lineitem_json = json.load(lineitem_file)
-        responses.add(responses.GET, '{}/sample/v1/projects/1/lineItems/100'.format(BASE_HOST), json=lineitem_json, status=200)
-        self.api.get_lineitem(1, 100)
+    def test_get_line_item(self):
+        with open('./tests/test_files/get_line_item.json', 'r') as line_item_file:
+            line_item_json = json.load(line_item_file)
+        responses.add(responses.GET, '{}/sample/v1/projects/1/lineItems/100'.format(BASE_HOST), json=line_item_json, status=200)
+        self.api.get_line_item(1, 100)
         self.assertEqual(len(responses.calls), 1)
-        self.assertEqual(responses.calls[0].response.json(), lineitem_json)
+        self.assertEqual(responses.calls[0].response.json(), line_item_json)
+
+    @responses.activate
+    def test_get_line_item_detailed_report(self):
+        with open('./tests/test_files/get_line_item_detailed_report.json', 'r') as line_item_detailed_report_file:
+            line_item_detailed_report_json = json.load(line_item_detailed_report_file)
+        responses.add(responses.GET, '{}/sample/v1/projects/1/lineItems/100/detailedReport'.format(BASE_HOST), json=line_item_detailed_report_json, status=200)
+        self.api.get_line_item_detailed_report(1, 100)
+        self.assertEqual(len(responses.calls), 1)
+        self.assertEqual(responses.calls[0].response.json(), line_item_detailed_report_json)
