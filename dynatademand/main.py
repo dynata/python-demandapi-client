e2926ed1f76b4e7797f958e7f67ad3d96ca1f4de
huy <huyvanvo94@gmail.com>
2020-01-23 12:38:55 -0800

    finish with get_invoices_summary

--- dynatademand/main.py
+++
@@ -1,8 +0,0 @@
-from dynatademand.api import DemandAPIClient
-
-api = DemandAPIClient(client_id='api', username='test', password='test', base_host='https://api.dev.pe.dynata.com')
-api.authenticate()
-
-result = api.get_invoices_summary(startDate='2019-06-12', endDate='2019-06-19', extProjectId='010528ef-8984-48c1-a06d-4dae730da027')
-
-print(result)
